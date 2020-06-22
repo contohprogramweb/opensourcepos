110712009d26c0a492fb15dbd72adb3db8368c85
objecttothis <objecttothis@gmail.com>
2020-06-22 11:10:37 +0400

    Formatting and code cleanup
    - Added NEW_ITEM constant
    - Renamed variables for clarity
    - Tab indentation of equals signs for easy of reading
    - Removed unnecessary comments
    - Removed unecessary variable.
    - Renamed CSV headings to for consistency with other headings
    - Remove method chaining for clarity.
    - Replaced array() and new Array() with [] for speed (+20%) and to match
    current standard.
    - Removed trailing whitespace and whitespace on empty lines

--- application/models/Item.php
+++ application/models/Item.php
@@ -545,7 +545,7 @@ class Item extends CI_Model
 
 	public function get_search_suggestions($search, $filters = array('is_deleted' => FALSE, 'search_custom' => FALSE), $unique = FALSE, $limit = 25)
 	{
-		$suggestions = array();
+		$suggestions = [];
 		$non_kit = array(ITEM, ITEM_AMOUNT_ENTRY);
 
 		$this->db->select($this->get_search_suggestion_format('item_id, name, pack_name'));
@@ -644,7 +644,7 @@ class Item extends CI_Model
 
 	public function get_stock_search_suggestions($search, $filters = array('is_deleted' => FALSE, 'search_custom' => FALSE), $unique = FALSE, $limit = 25)
 	{
-		$suggestions = array();
+		$suggestions = [];
 		$non_kit = array(ITEM, ITEM_AMOUNT_ENTRY);
 
 		$this->db->select($this->get_search_suggestion_format('item_id, name, pack_name'));
@@ -745,7 +745,7 @@ class Item extends CI_Model
 
 	public function get_kit_search_suggestions($search, $filters = array('is_deleted' => FALSE, 'search_custom' => FALSE), $unique = FALSE, $limit = 25)
 	{
-		$suggestions = array();
+		$suggestions = [];
 		$non_kit = array(ITEM, ITEM_AMOUNT_ENTRY);
 
 		$this->db->select('item_id, name');
@@ -854,7 +854,7 @@ class Item extends CI_Model
 
 	public function get_low_sell_suggestions($search)
 	{
-		$suggestions = array();
+		$suggestions = [];
 
 		$this->db->select($this->get_search_suggestion_format('item_id, pack_name'));
 		$this->db->from('items');
@@ -872,7 +872,7 @@ class Item extends CI_Model
 
 	public function get_category_suggestions($search)
 	{
-		$suggestions = array();
+		$suggestions = [];
 		$this->db->distinct();
 		$this->db->select('category');
 		$this->db->from('items');
@@ -889,7 +889,7 @@ class Item extends CI_Model
 
 	public function get_location_suggestions($search)
 	{
-		$suggestions = array();
+		$suggestions = [];
 		$this->db->distinct();
 		$this->db->select('location');
 		$this->db->from('items');
